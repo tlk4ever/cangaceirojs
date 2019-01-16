89c2e5d643ef2df2ec8db8dfc3d699ae2889eeb5
Thiago Brito <tbffreitas@gmail.com>
2019-01-16 21:05:48 -0200

    class View e herança

---
+++ client/app/ui/views/View.js
@@ -0,0 +1,10 @@
+class View{
+    
+    constructor (seletor){
+        this._elemento = document.querySelector(seletor);    
+    }
+    
+    update(model){
+        this._elemento.innerHTML = this.template(model);    
+    }
+}
