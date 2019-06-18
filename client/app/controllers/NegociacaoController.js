621c2f26847703777a5660fe3680d507c80e673c
Thiago Brito de Freitas <thiago.freitas@cruzeiro.com.br>
2019-06-18 09:58:25 -0300

    fim 10.3

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -7,41 +7,25 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            
-            get(target, prop, receiver) {
-            
-                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia']
-                    .includes(prop)) {
-                        
-                        return function() {
+        this._negociacoes = new Bind (
+            new Negociacoes(),
+            new NegociacoesView('#negociacoes'),
+            ['adiciona','esvazia']
+        );
 
-                            console.log(`"${prop}" disparou a armadilha`);
-                            target[prop].apply(target, arguments);
-                            self._negociacoesView.update(target);
-                        }
-
-                } else {
-
-                    return target[prop];
-                }
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView('#negociacoes');
-        this._negociacoesView.update(this._negociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind (
+            new Mensagem(),
+            new MensagemView('#mensagemView'),
+            ['texto']
+        );
     }
+
     adiciona(event) {
 
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();
     }
 
@@ -66,6 +50,6 @@ class NegociacaoController {
 
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
+        
     }
 }
